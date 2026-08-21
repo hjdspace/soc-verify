6e003fb1c15beeb02767044abdf8265bc902186d
hjdspace <hjdspace@qq.com>
2026-08-21 16:44:13 +0800

    Add stub for ModelRegistry class

---
+++ runner/engine-stubs/model-registry.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// Stub for engine/oh-my-pi/packages/coding-agent/src/config/model-registry
+
+export class ModelRegistry {
+  constructor(_authStorage: unknown) {}
+  find(_provider: string, _modelId: string): any {
+    throw new Error('stub: should not be called at compile time');
+  }
+}
