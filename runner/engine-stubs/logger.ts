a7c5c52eb6e32656d7ffa2de99978be0f22f9bdb
hjdspace <hjdspace@qq.com>
2026-08-21 16:36:39 +0800

    Add logger stub with setTransports function

---
+++ runner/engine-stubs/logger.ts
@@ -0,0 +1,5 @@
+// Stub for engine/oh-my-pi/packages/utils/src/logger
+
+export function setTransports(_options: { console?: boolean; file?: boolean }): void {
+  throw new Error('stub: should not be called at compile time');
+}
